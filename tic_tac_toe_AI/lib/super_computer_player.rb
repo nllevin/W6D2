c6d89e7d17428dd3302ec2795a349ac805f566e5
nllevin <50681582+nllevin@users.noreply.github.com>
2019-08-06 13:16:22 -0400

    Complete SuperComputerPlayer, write #move

--- tic_tac_toe_AI/lib/super_computer_player.rb
+++ tic_tac_toe_AI/lib/super_computer_player.rb
@@ -2,6 +2,17 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    non_losing_moves = []
+
+    node.children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+      non_losing_moves << child.prev_move_pos unless child.losing_node?(mark)
+    end
+
+    return non_losing_moves.sample unless non_losing_moves.empty?
+
+    raise "This is impossible; I never lose!"
   end
 end
 
